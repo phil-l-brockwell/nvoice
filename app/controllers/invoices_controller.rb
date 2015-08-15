e5233d2a3186e84f305230645bc4eb841e1c74c4
Phil Brockwell <nineteen40eight@gmail.com>
2015-08-16 00:49:02 +0100

    added erb template to invoice pdf

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,11 +1,12 @@
 class InvoicesController < ApplicationController
 
   def create
-    invoice = Invoice.create(invoice_params)
-    if invoice.save
-      pdf = WickedPdf.new.pdf_from_string('app/views/layouts/invoice.html')
-      InvoiceMailer.client_email(invoice, pdf).deliver_later
-      InvoiceMailer.business_email(invoice, pdf).deliver_later
+    @invoice = Invoice.create(invoice_params)
+    if @invoice.save
+      html = render_to_string("invoice.html.erb", layout: false)
+      pdf = WickedPdf.new.pdf_from_string(html)
+      InvoiceMailer.client_email(@invoice, pdf).deliver_later
+      InvoiceMailer.business_email(@invoice, pdf).deliver_later
       head 200
     else
       head 500
