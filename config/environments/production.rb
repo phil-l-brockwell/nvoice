f6fe4ff1cae7facba0b05780d0bffb27fb0493fc
Phil Brockwell <nineteen40eight@gmail.com>
2015-08-15 18:48:53 +0100

    changed domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'example.com',
+    domain: 'n-voice-app.herokuapp.com',
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_USERNAME'],
