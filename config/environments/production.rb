e10667d11aa6798130d1ef28b0cbca9a8574d20e
Phil Brockwell <nineteen40eight@gmail.com>
2015-08-15 18:18:27 +0100

    added production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.mailgun.org',
+    port:            587,
+    domain:          ENV['domain'],
+    user_name:       ENV['username'],
+    password:        ENV['password'],
+    authentication: :plain
+  }
+
+  config.action_mailer.default_url_options = { host: 'n-voice-app.herokuapp.com' }
 end
